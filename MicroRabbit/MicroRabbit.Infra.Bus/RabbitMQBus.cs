79a20a20ee3ce17dc4738678181340d086b2ad39
junj0619@gmail.com <junj0619@gmail.com>
2020-11-22 19:11:16 -0500

    Implemented Transfef MicroService to subscrible RabbitMQ TransferCreated Envent

--- MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -2,6 +2,7 @@
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Domain.Core.Commands;
 using MicroRabbit.Domain.Core.Events;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -19,12 +20,13 @@ namespace MicroRabbit.Infra.Bus
         private readonly IMediator _mediator;
         private readonly Dictionary<string, List<Type>> _handlers;
         private readonly List<Type> _eventTypes;
-
-        public RabbitMQBus(IMediator mediator)
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        public RabbitMQBus(IMediator mediator, IServiceScopeFactory serviceScopeFactory)
         {
             _mediator = mediator;
             _handlers = new Dictionary<string, List<Type>>();
             _eventTypes = new List<Type>();
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         public void Publish<T>(T @event) where T : Event
@@ -108,20 +110,41 @@ namespace MicroRabbit.Infra.Bus
             }
         }
 
+        //OLD CODE only take paramtereless constructor of hanlder
+        //private async Task ProcessEvent(string eventName, string message)
+        //{
+        //    if (_handlers.ContainsKey(eventName))
+        //    {
+        //        var subscriptions = _handlers[eventName];
+        //        foreach (var subscription in subscriptions)
+        //        {
+        //            var handler = Activator.CreateInstance(subscription);
+        //            if (handler == null) continue;
+        //            var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+        //            var @event = JsonConvert.DeserializeObject(message, eventType);
+        //            var conreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+        //            await (Task)conreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+        //        }
+        //    }
+        //}
+
         private async Task ProcessEvent(string eventName, string message)
         {
             if (_handlers.ContainsKey(eventName))
             {
-                var subscriptions = _handlers[eventName];
-                foreach (var subscription in subscriptions)
+                using (var scope = _serviceScopeFactory.CreateScope())
                 {
-                    var handler = Activator.CreateInstance(subscription);
-                    if (handler == null) continue;
-                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
-                    var @event = JsonConvert.DeserializeObject(message, eventType);
-                    var conreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
-                    await (Task)conreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
-                }
+                    var subscriptions = _handlers[eventName];
+                    foreach (var subscription in subscriptions)
+                    {
+                        var handler = scope.ServiceProvider.GetService(subscription);
+                        if (handler == null) continue;
+                        var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                        var @event = JsonConvert.DeserializeObject(message, eventType);
+                        var conreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                        await (Task)conreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+                    }
+                }               
             }
         }
     }
